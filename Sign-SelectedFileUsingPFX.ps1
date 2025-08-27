c1855d716f30cd56d3b66bf6fb17b00dfe28f0ef
tvbg\b.popov <b.popov@team-vision.bg>
2025-08-27 14:08:47 +0300

    Fix typo in Add-Type assembly name in Sign-SelectedFileUsingPFX.ps1

--- Sign-SelectedFileUsingPFX.ps1
+++ Sign-SelectedFileUsingPFX.ps1
@@ -1,5 +1,5 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Add-Type -AssemblyName PresentationCore, PresentationFramework, System.Windows.Forms
+Add-Type -AssemblyName PresentationCore, PresentationFramework, System.Windows.FormstionCore, PresentationFramework, System.Windows.Forms
 
 [xml]$XAML = @"
 <Window
